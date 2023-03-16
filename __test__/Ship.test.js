3410f9f58c68538f642f5217bc5e49d68a40e346
Benj <ben.speight@outlook.com>
2023-03-16 20:40:24 +0000

    set sail

--- __test__/Ship.test.js
+++ __test__/Ship.test.js
@@ -7,7 +7,19 @@ describe('Ship', () => {
 
     it('has a starting port', () => {
         const ship = new Ship('Holyhead')
-        
+
         expect(ship.startingPort).toBe('Holyhead');
     });
+
+    it('can set sail', () => {
+        const ship = new Ship('Holyhead')
+
+        ship.setSail();
+
+        expect(ship.startingPort).toBeFalsy();
+    });
+
+
+
+
 });
--- src/Ship.js
+++ src/Ship.js
@@ -1,7 +1,15 @@
-function Ship(startingPort){
-    this.startingPort = startingPort;
 
+
+class Ship {
+    constructor(startingPort) {
+        this.startingPort = startingPort;
+    }
+
+    setSail() {
+        this.startingPort = null;
+    }
 };
+
 
 
 module.exports = Ship;
