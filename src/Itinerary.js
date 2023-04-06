3645191f8baf6ee6a06bc7a625c7b483ee71adeb
Benj <ben.speight@outlook.com>
2023-04-06 21:06:49 +0100

    working progress add port

--- src/Itinerary.js
+++ src/Itinerary.js
@@ -1,9 +1,11 @@
-(function Itinerary (ports) {
-    this.ports = ports;    
-    
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Itinerary;
-      } else {
-        window.Itinerary = Itinerary;
-      }
-}());
+(function exportItinerary() {
+  function Itinerary(ports) {
+    this.ports = ports;
+  }
+
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = Itinerary;
+  } else {
+    window.Itinerary = Itinerary;
+  }
+})();
--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,7 @@
   function Controller(ship) {
     this.ship = ship;
     this.initialiseSea();
+    this.renderHud();
 
     document.querySelector("#sailbutton").addEventListener("click", () => {
       this.setSail();
@@ -38,9 +39,7 @@
 
     renderShip() {
       const ship = this.ship;
-      const shipPortIndex = this.ship.itinerary.ports.indexOf(
-        this.ship.currentPort
-      );
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
       const portElement = document.querySelector(
         `[data-port-index='${shipPortIndex}']`
       );
@@ -61,18 +60,50 @@
       );
 
       if (!nextPortElement) {
-        return alert("End of the line");
+        this.renderMessage("End of the line!");
+      } else if (ship.currentPort !== null) {
+        ship.setSail();
+        const nextPortName = ship.itinerary.ports[nextPortIndex - 1].name;
+        this.renderMessage(`Now departing ${nextPortName}`);
       }
+
       const shipElement = document.querySelector("#ship");
       const sailInterval = setInterval(() => {
         const shipLeft = parseInt(shipElement.style.left, 10);
         if (shipLeft === nextPortElement.offsetLeft - 32) {
           ship.setSail();
           ship.dock();
+          this.renderMessage(`Arriving at ${ship.currentPort.name}`);
           clearInterval(sailInterval);
         }
         shipElement.style.left = `${shipLeft + 1}px`;
       }, 20);
+    },
+
+    renderMessage(message) {
+      const messageElement = document.createElement("div");
+      messageElement.id = "message";
+      messageElement.innerHTML = message;
+      const viewport = document.querySelector("#viewport");
+
+      viewport.appendChild(messageElement);
+
+      setTimeout(() => {
+        viewport.removeChild(messageElement);
+      }, 2000);
+    },
+
+    renderHud() {
+      const hudElement = document.createElement("div");
+      hudElement.id = "hud";
+      const body = document.querySelector("body");
+
+      window.setInterval(() => {
+        const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+        const nextPortIndex = currentPortIndex + 1;
+        hudElement.innerHTML = `Current Port: ${ship.currentPort.name} <br/> Next Port: ${ship.itinerary.ports[nextPortIndex].name}`;
+        body.appendChild(hudElement);
+      }, 1000);
     },
   };
 
